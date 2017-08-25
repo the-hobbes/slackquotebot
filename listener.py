0248439b6aab63a27964aa9658f6bb2744aaa0e5
Phelan Vendeville <phelan.vendeville@gmail.com>
2017-08-24 21:43:17 -0700

    Work towards parsing slack input for quotebot.
    This changelist implements the parse_slack_input function of the listener module
    as well as sets up a unittest for the same. It also includes a refactor of the
    command line argument parsing into a function, and the removal of an unnecessary
    arg there.
    
    The gitignore file has also been updated to ignore "secrets", so I don't
    accidentally upload the API key for the bot or something important like that.

--- listener.py
+++ listener.py
@@ -6,29 +6,14 @@
   - trigger handlers based on those commands
 """
 
-# TODO: import necessary slack client libraries
-# TODO: declare necessary constants
 import argparse
 import quotebot
+import time
 from slackclient import SlackClient
 
-# Pull the bot id and bot token from command line arguments
-parser = argparse.ArgumentParser(description='listen for bot commands on slack')
-parser.add_argument('--bot_id', type=str, help='the ID of the slack chatbot')
-parser.add_argument(
-  '--slack_bot_token', type=str, help='the Token of the slack chatbot')
-args = parser.parse_args()
-
 # Set constants
-BOT_ID = args.bot_id
-SLACK_BOT_TOKEN = args.slack_bot_token
 READ_WEBSOCKET_DELAY = 1 # 1 second delay between sampling the firehose
-
-# Raise an error if a required flag isn't set
-if not BOT_ID:
-  parser.error('Bot ID not given')
-if not SLACK_BOT_TOKEN:
-  parser.error('Slack Bot Token not given')
+COMMANDS = ['!quote', '!addquote', '!deletequote']
 
 
 def handle_command(command, channel):
@@ -45,10 +30,10 @@ def handle_command(command, channel):
     The responses constructed by the quotebot layer are sent to the appropriate
     slack channel.
   """
-  pass
+  print(command, channel)
 
 
-def parse_slack_input():
+def parse_slack_input(slack_rtm_output):
   """Decides if a message has been directed at the bot.
 
     Takes the firehose of information from the Slack Real Time Messaging API and
@@ -64,11 +49,36 @@ def parse_slack_input():
     - command (string) the command directed at the bot
     - channel (string) the channel the command originated from
  """
-  pass
+  output_list = slack_rtm_output
+  if output_list and len(output_list) > 0:
+    for output in output_list:
+      if 'text' in output and any(cmd in output['text'] for cmd in COMMANDS):
+        return output['text'], output['channel']
+
+  return None, None
+
+
+def parse_arguments():
+  """Pull the bot token from the command line.
+
+    Returns:
+    - slack_bot_token (string) the bot api access token
+ """
+  parser = argparse.ArgumentParser(
+    description='listen for bot commands on slack')
+  parser.add_argument(
+    '--slack_bot_token', type=str, help='the Token of the slack chatbot')
+  args = parser.parse_args()
+  slack_bot_token = args.slack_bot_token
+  if not slack_bot_token:
+    parser.error('Slack Bot Token not given')
+
+  return slack_bot_token
 
 
 def main():
-  slack_client = SlackClient(SLACK_BOT_TOKEN)
+  slack_bot_token = parse_arguments()
+  slack_client = SlackClient(slack_bot_token)
   if slack_client.rtm_connect():
     print("Quotebot is connected and running.")
     # infinite loop to continuously consume slack data from rtm api
--- listener_test.py
+++ listener_test.py
@@ -12,7 +12,15 @@ class TestListener(unittest.TestCase):
     pass
 
   def test_parse_slack_input(self):
-    pass
-  
+    fake_input = [{
+    u'source_team': u'SOMETEAM', u'text': u'!quote blah blah',
+    u'ts': u'1503633307.000114', u'user': u'SOMEUSER', u'team': u'SOMETEAM',
+    u'type': u'message', u'channel': u'SOMECHANNEL'}]
+    expected_command, expected_channel = u'!quote blah blah', u'SOMECHANNEL'
+    received_command, received_channel = listener.parse_slack_input(fake_input)
+    self.assertEqual(expected_command, received_command)
+    self.assertEqual(expected_channel, received_channel)
+
+
 if __name__ == '__main__':
     unittest.main()
