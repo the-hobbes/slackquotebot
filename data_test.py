fc791108df7348474612571892303dd16337ae83
Phelan Vendeville <phelan.vendeville@gmail.com>
2017-09-07 19:44:37 -0700

    Add unit tests for the data.py module.
    In accordance with issue #6, this change adds some unit tests for data.py. It
    mocks the data.connect function by returning instances of a fake class with
    methods setup to return known values rather than interacting w/the db. Both
    failure and success of each of the three operations are tested.

--- data_test.py
+++ data_test.py
@@ -1,33 +1,81 @@
 """Tests for data.py"""
 
 import data
+import logging
+import mock
+import psycopg2
+import settings
 import unittest
-import psycopg2 
+
 from mock import MagicMock
+
+
+class MockConnection():
+
+  def execute(self, *args, **kwargs):
+    return True
+
+  def fetchone(self, *args, **kwargs):
+    return ["I am a quote"]
+
+  def close(self):
+    return None
+
+  def commit(self):
+    return True
 
 
 class TestData(unittest.TestCase):
 
-	def setup():
-		pass
+  def setUp(self):
+    logging.disable(logging.ERROR)  # suppress log output for tests
+    psycopg2.connect = MockConnection()
+    self.conn = psycopg2.connect
+    settings.QUOTE_LIST = [1]
+
+  def tearDown(self):
+    self.conn = None
 
   def test_retrieve(self):
-    pass
+    expected_response = "I am a quote"
+    data.connect = MagicMock(return_value=[self.conn, self.conn])
+    received_response = data.retrieve()
+    self.assertEqual(expected_response, received_response)
 
   def test_retrieve_fails(self):
-    pass
-
+    expected_response = -1
+    data.connect = MagicMock(return_value=[self.conn, self.conn])
+    self.conn.fetchone = MagicMock(side_effect=psycopg2.Error)
+    received_response = data.retrieve()
+    self.assertEqual(expected_response, received_response)
+  
   def test_save(self):
-    pass
+    expected_response = 2
+    data.connect = MagicMock(return_value=[self.conn, self.conn])
+    data.prefetch_quote_ids = MagicMock()
+    self.conn.fetchone = MagicMock(return_value=[2])
+    received_response = data.save("I am a quote!")
+    self.assertEqual(expected_response, received_response)
 
   def test_save_fails(self):
-    pass
+    expected_response = -1
+    data.connect = MagicMock(return_value=[self.conn, self.conn])
+    self.conn.fetchone = MagicMock(side_effect=psycopg2.Error)
+    received_response = data.save("I am a quote!")
+    self.assertEqual(expected_response, received_response)
 
   def test_delete(self):
-    pass
+    expected_response = True
+    data.connect = MagicMock(return_value=[self.conn, self.conn])
+    received_response = data.delete(1)
+    self.assertEqual(expected_response, received_response)
 
   def test_delete_fails(self):
-    pass
+    expected_response = False
+    data.connect = MagicMock(return_value=[self.conn, self.conn])
+    self.conn.commit = MagicMock(side_effect=psycopg2.Error)
+    received_response = data.delete(1)
+    self.assertEqual(expected_response, received_response)
 
 if __name__ == '__main__':
     unittest.main()
